dbae68267ed99f8829d4a456366a4b1f65e6ea3b
Onyxlotus <Onyxlotus@yandex.ru>
2025-01-27 11:31:22 +0300

    php script

--- script.js
+++ script.js
@@ -29,10 +29,10 @@ document.addEventListener('scroll', function() {
     const currentScroll = window.scrollY;
     const sectionOffsets = Array.from(sections).map(section => section.offsetTop);
 
-    if (currentScroll > sectionOffsets[currentSectionIndex] + sections[currentSectionIndex].offsetHeight / 1.5) {
+    if (currentScroll > sectionOffsets[currentSectionIndex] + sections[currentSectionIndex].offsetHeight / 2) {
         currentSectionIndex = Math.min(currentSectionIndex + 1, sections.length - 1);
-        sections[currentSectionIndex].scrollIntoView({ behavior: 'smooth', block: 'start' });
-    } else if (currentScroll < sectionOffsets[currentSectionIndex] - sections[currentSectionIndex].offsetHeight / 1.5) {
+        sections[currentSectionIndex].scrollIntoView({ behavior: 'smooth', block: 'end' });
+    } else if (currentScroll < sectionOffsets[currentSectionIndex] - sections[currentSectionIndex].offsetHeight / 2) {
         currentSectionIndex = Math.max(currentSectionIndex - 1, 0);
         sections[currentSectionIndex].scrollIntoView({ behavior: 'smooth', block: 'end' });
     }
@@ -59,79 +59,10 @@ let slider = document.querySelector('.slider'),
     sliderTrack.style.transition = 'transform .5s';
     sliderTrack.style.transform = `translate3d(-${slideIndex * slideWidth}px, 0px, 0px)`;
 
-    // делаем стрелку prev недоступной на первом слайде
-    // и доступной в остальных случаях
-    // делаем стрелку next недоступной на последнем слайде
-    // и доступной в остальных случаях
+
     prev.classList.toggle('disabled', slideIndex === 0);
     next.classList.toggle('disabled', slideIndex === --slides.length);
   } 
-
-  getEvent = function() {
-    return event.type.search('touch') !== -1 ? event.touches[0] : event;
-    // p.s. event - аргумент по умолчанию в функции
-  },
-  // или es6
-  getEvent = () => event.type.search('touch') !== -1 ? event.touches[0] : event,
-  
-  swipeStart = function() {
-    let evt = getEvent();
-  
-    // берем начальную позицию курсора по оси Х
-    posInit = posX1 = evt.clientX;
-  
-    // убираем плавный переход, чтобы track двигался за курсором без задержки
-    // т.к. он будет включается в функции slide()
-    sliderTrack.style.transition = '';
-  
-    // и сразу начинаем отслеживать другие события на документе
-    document.addEventListener('touchmove', swipeAction);
-    document.addEventListener('touchend', swipeEnd);
-    document.addEventListener('mousemove', swipeAction);
-    document.addEventListener('mouseup', swipeEnd);
-  },
-  swipeAction = function() {
-    let evt = getEvent(),
-      // для более красивой записи возьмем в переменную текущее свойство transform
-      style = sliderTrack.style.transform,
-      // считываем трансформацию с помощью регулярного выражения и сразу превращаем в число
-      transform = +style.match(trfRegExp)[0];
-  
-    posX2 = posX1 - evt.clientX;
-    posX1 = evt.clientX;
-  
-    sliderTrack.style.transform = `translate3d(${transform - posX2}px, 0px, 0px)`;
-    // можно было бы использовать метод строк .replace():
-    // sliderTrack.style.transform = style.replace(trfRegExp, match => match - posX2);
-    // но в дальнейшем нам нужна будет текущая трансформация в переменной
-  }
-
-  swipeEnd = function() {
-    // финальная позиция курсора
-    posFinal = posInit - posX1;
-  
-    document.removeEventListener('touchmove', swipeAction);
-    document.removeEventListener('mousemove', swipeAction);
-    document.removeEventListener('touchend', swipeEnd);
-    document.removeEventListener('mouseup', swipeEnd);
-  
-    // убираем знак минус и сравниваем с порогом сдвига слайда
-    if (Math.abs(posFinal) > posThreshold) {
-      // если мы тянули вправо, то уменьшаем номер текущего слайда
-      if (posInit < posX1) {
-        slideIndex--;
-      // если мы тянули влево, то увеличиваем номер текущего слайда
-      } else if (posInit > posX1) {
-        slideIndex++;
-      }
-    }
-  
-    // если курсор двигался, то запускаем функцию переключения слайдов
-    if (posInit !== posX1) {
-      slide();
-    }
-  
-  };
 
   arrows.addEventListener('click', function() {
     let target = event.target;
